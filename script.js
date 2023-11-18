7fa0eb811888f00925ea3cb2ec511e9026217be5
csongam7 <csongam7@gmail.com>
2023-11-18 16:14:34 +0100

    adding working point counter for Player VS Computer game point counter only working when it needs to

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 console.log("Wello Horld")
 
-
+let playerScore = 0;
+let computerScore = 0;
+let gameIsOn = false;
 /* Make 3 button for the user with the texts: "Rock", "Paper", "Scissor" */
 const rockButton = document.querySelector("#Rock");
 const paperButton = document.querySelector("#Paper");
 const scissorButton = document.querySelector("#Scissor");
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".button");
+const roundButton = document.querySelector('#roundButton');
 
+roundButton.addEventListener("click", function() {
+    gameIsOn = true;
+});
 
-buttons.forEach((button) =>
-    button.addEventListener("click", function(){
-        const computersChoice = getComputerChoice();
-        
-        if (button.innerText == computersChoice){
-            alert(`It's a tie, the computer picket ${computersChoice} as well`);
-            return;
-        }
+    buttons.forEach((button) =>
+        button.addEventListener("click", function round(){
+            const computersChoice = getComputerChoice();
+                if (button.innerText == computersChoice){
+                    alert(`It's a tie, the computer picket ${computersChoice} as well`);
+                    return;
+                }
 
-        if (button.innerText == "Rock" &&  computersChoice != "Paper" && computersChoice != button.innerText){
-            alertMessage(true, computersChoice);
-        }
+                if (button.innerText == "Rock" &&  computersChoice != "Paper" && computersChoice != button.innerText){
+                    if(gameIsOn){
+                    gameCounter(checkWhoWon(true, computersChoice))
+                    return;
+                    }
+                    checkWhoWon(true, computersChoice);
+                    return;
+                }
 
-        else if(button.innerText == "Paper" && computersChoice != "Scissor"){
-            alertMessage(true, computersChoice);
-        }
+                else if(button.innerText == "Paper" && computersChoice != "Scissor"){
+                    if(gameIsOn){
+                    gameCounter(checkWhoWon(true, computersChoice))
+                    return;
+                    }
+                    checkWhoWon(true, computersChoice);
+                    return;
+                }
 
-        else if(button.innerText == "Scissor" && computersChoice != "Rock"){
-            alertMessage(true, computersChoice);
-        }
-
-        else {alertMessage(false, computersChoice);}
-    }
+                else if(button.innerText == "Scissor" && computersChoice != "Rock"){
+                    if(gameIsOn){
+                    gameCounter(checkWhoWon(true, computersChoice))
+                    return;
+                    }
+                    checkWhoWon(true, computersChoice);
+                    return;
+                }
+                else
+                if(gameIsOn){gameCounter(checkWhoWon(false, computersChoice))
+                    return;
+                }
+                checkWhoWon(true, computersChoice);
+            }
+        )
     )
-    );
 
 /* After his choice make the computer choose randomly of the same 3*/
 function getComputerChoice(){
@@ -41,13 +64,38 @@ function getComputerChoice(){
     return computerChoice;
 }
 /*Alert the user the result.*/
-function alertMessage(didIwin, computersChoice){
+function checkWhoWon(didIwin, computersChoice){
     if (didIwin){
         alert(`You are the winner the computer has choosen ${computersChoice}`);
+        return "Player";
     }
     else {
         alert(`You have been beaten by the computer whose pick was ${computersChoice}`);
+        return "Computer";
     }
 };
 
-
+function gameCounter(whoScored){
+    
+    if (whoScored == "Player"){
+        playerScore ++;
+    }
+    else if (whoScored == "Computer"){
+        computerScore ++;
+    }
+    
+    if (playerScore < 3 && computerScore < 3){
+        alert(`Current score: Player:${playerScore} - Computer:${computerScore}`)
+        return
+    }
+    
+    if (playerScore > computerScore){
+        alert(`congratulations you have beaten the computer in Rock Paper Scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`)
+    }
+    else if (computerScore > playerScore){
+        alert(`You have been beaten by the computer in Rock Paper Scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`)
+    }
+    playerScore = 0;
+    computerScore = 0;
+    gameIsOn = false;
+}
