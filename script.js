5f2af821ec45554a1fbf8139592ee19ab0eea2b0
csongam7 <csongam7@gmail.com>
2023-12-15 10:04:01 +0100

    first well working state

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-console.log("Wello Horld")
-
 let playerScore = 0;
 let computerScore = 0;
 let gameIsOn = false;
-/* Make 3 button for the user with the texts: "Rock", "Paper", "Scissor" */
-const rockButton = document.querySelector("#Rock");
-const paperButton = document.querySelector("#Paper");
-const scissorButton = document.querySelector("#Scissor");
+/* Make 3 button for the user with the texts: "rock", "paper", "scissor" */
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissor");
 const buttons = document.querySelectorAll(".button");
 const roundButton = document.querySelector('#roundButton');
-const messageToThePlayer = document.querySelector('#message_to_the_player');
+let messageToThePlayer = document.querySelector('#message_to_the_player');
+const battlefield = document.querySelector('#battlefield');
 
-
+//makes the changing values go back to the default one.
+function setbackToDefault(){
+    setTimeout(function(){
+    playerScore = 0;
+    computerScore = 0;
+    gameIsOn = false;
+    document.querySelector('#main_scoreboard').innerHTML = '';
+    document.querySelector('#message_to_the_player').innerText = '';
+    document.querySelector('#computers_score_counter').classList.add('.hide');
+    document.querySelector('#players_score_counter').classList.add('.hide');
+    const smallerSymbols = document.querySelectorAll('.smaller_symbol');
+    const smallerSymbolsArray = [...smallerSymbols];
+    smallerSymbolsArray.forEach(smallerSymbol => smallerSymbol.src = "/images/question_mark.png");
+    }, 3000);  
+}
 
 
 roundButton.addEventListener("click", function() {
-    gameIsOn = true;
-    
     //initialize the socreboard when the player wants to play a 5 round game againts the computer
+    //setbackToDefault();
+    gameIsOn = true;
+    document.querySelector('#players_score_counter').innerText = 0;
+    document.querySelector('#computers_score_counter').innerText = 0;
+    document.querySelectorAll('.smaller_symbol').src="/images/question_mark.png";
+    /*battlefield.classList.add("show")
 
     const mainScoreboard = document.createElement('div');
     
@@ -28,11 +45,12 @@ roundButton.addEventListener("click", function() {
     const playersScore = document.createElement('span');
     playersScore.id = 'playersScore';
     
+    
     playersScoreboard.appendChild(playersName);
     playersScoreboard.appendChild(playersScore);
     mainScoreboard.appendChild(playersScoreboard);
 
-    //initialize computer's part of the socreboard
+    //initialize computer's part of the scoreboard
     const computersScoreboard = document.createElement('div');
     const computersName = document.createElement('span');
     computersName.innerText = 'Computer\'s score:';
@@ -45,68 +63,73 @@ roundButton.addEventListener("click", function() {
     mainScoreboard.appendChild(computersScoreboard);
     mainScoreboard.id = "main_scoreboard";
     document.body.appendChild(mainScoreboard);
-});
+*/});
 
     buttons.forEach((button) =>
         button.addEventListener("click", function round(){
             const computersChoice = getComputerChoice();
-                if (button.innerText == computersChoice){
-                    alert(`It's a tie, the computer picket ${computersChoice} as well`);
+                if (button.id == computersChoice){
+                    displayWhoWon("tie", computersChoice, button.id);
                     return;
                 }
 
-                if (button.innerText == "Rock" &&  computersChoice != "Paper" && computersChoice != button.innerText){
+                if (button.id == "rock" &&  computersChoice != "paper"){
                     if(gameIsOn){
-                    gameCounter(checkWhoWon(true, computersChoice))
+                    gameCounter(displayWhoWon("player", computersChoice, button.id))
                     return;
                     }
-                    checkWhoWon(true, computersChoice);
+                    displayWhoWon("player", computersChoice, button.id);
                     return;
                 }
 
-                else if(button.innerText == "Paper" && computersChoice != "Scissor"){
+                else if(button.id == "paper" && computersChoice != "scissor"){
                     if(gameIsOn){
-                    gameCounter(checkWhoWon(true, computersChoice))
+                    gameCounter(displayWhoWon("player", computersChoice, button.id))
                     return;
                     }
-                    checkWhoWon(true, computersChoice);
+                    displayWhoWon("player", computersChoice, button.id);
                     return;
                 }
 
-                else if(button.innerText == "Scissor" && computersChoice != "Rock"){
+                else if(button.id == "scissor" && computersChoice != "rock"){
                     if(gameIsOn){
-                    gameCounter(checkWhoWon(true, computersChoice))
+                    gameCounter(displayWhoWon("player", computersChoice, button.id))
                     return;
                     }
-                    checkWhoWon(true, computersChoice);
+                    displayWhoWon("player", computersChoice, button.id);
                     return;
                 }
-                else
-                if(gameIsOn){gameCounter(checkWhoWon(false, computersChoice))
+                else if(gameIsOn){
+                    gameCounter(displayWhoWon("computer", computersChoice, button.id))
                     return;
                 }
-                checkWhoWon(true, computersChoice);
+                displayWhoWon("computer", computersChoice, button.id);
             }
         )
     )
 
 // After his choice make the computer choose randomly of the same 3
 function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissor"];
+    const choices = ["rock", "paper", "scissor"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 //Alert the user the result.
-function checkWhoWon(didIwin, computersChoice){
-    if (didIwin){
-        //alert(`You are the winner the computer has choosen ${computersChoice}`);
-        messageToThePlayer.innerText = (`You are the winner the computer has choosen ${computersChoice}`);
+function displayWhoWon(result, computersChoice, playersChoice){
+    /**/
+    document.querySelector("#players_symbol").src=`/images/${playersChoice}.png`; 
+    document.querySelector("#computers_symbol").src= `/images/${computersChoice}.png`;
+    if (result == "player"){
+        messageToThePlayer.innerText = (`You won the computer has choosen ${computersChoice}.`);
         return "Player";
+        
     }
-    else {
-        //alert(`You have been beaten by the computer whose pick was ${computersChoice}`);
-        messageToThePlayer.innerText = (`You have been beaten by the computer whose pick was ${computersChoice}`);
+    else if (result == "computer"){
+        messageToThePlayer.innerText = (`You have been beaten by the computer whose choice was ${computersChoice}.`);
         return "Computer";
+    }
+    else{
+        messageToThePlayer.innerText = (`It's a tie the computer has picked ${computersChoice} as well.`);
     }
 };
 
@@ -115,36 +138,22 @@ function gameCounter(whoScored){
 
     if (whoScored == "Player"){
         playerScore ++;
-        document.querySelector('#playersScore').innerText = playerScore;
+        document.querySelector('#players_score_counter').innerText = playerScore;
     }
     else if (whoScored == "Computer"){
         computerScore ++;
-        document.querySelector('#computersScore').innerText = computerScore;
-    }
-    
-    if (playerScore + computerScore < 5){
-        return;
-    }
-    
-    if (playerScore > computerScore){
-        alert(`congratulations you have beaten the computer in Rock Paper Scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`)
-    }
-    else if (computerScore > playerScore){
-        alert(`You have been beaten by the computer in Rock Paper Scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`)
-    }
-    
-    
-    //makes the changing values go back to the default one adter the 5 round is over
-    setbackToDefault();
-
-    function setbackToDefault(){
-        playerScore = 0;
-        computerScore = 0;
-        gameIsOn = false;
-        document.querySelector('#main_scoreboard').innerHTML = '';
-        document.querySelector('#message_to_the_player').innerText = '';
+        document.querySelector('#computers_score_counter').innerText = computerScore;
+        console.log(document.querySelector('#computers_score'));
     }
 
-    
-
+    if (playerScore + computerScore == 5){
+        if (playerScore > computerScore){
+            messageToThePlayer.innerText = `congratulations you have beaten the computer in rock paper scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`;
+            console.log(document.querySelector('#computers_score').innerText, document.querySelector('#players_score').innerText);
+        }
+        else if (computerScore > playerScore){
+            messageToThePlayer.innerText = `You have been beaten by the computer in rock paper scissor! The final score is: Player:${playerScore} - Computer:${computerScore}`;
+        }
+        setbackToDefault();
+    }
 }
